dab343c717011c0605de3f487ceb3e40ade4d9f7
Givanildo Cardoso <givanildocardosotavares@gmail.com>
2026-09-11 19:28:09 -0300

    Adiciona multiplicação/divisão

--- calculadora.py
+++ calculadora.py
@@ -11,3 +11,15 @@ def subtracao(num1, num2):
     return subtracao
 
 print("Resultado da subtração: ", subtracao(89,33)) 
+
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+
+print("Resultado da multiplicação: ", multiplicacao(5, 6))
+
+def divisao(num1, num2):
+    divisao = num1 / num2
+    return divisao
+
+print("Resultado da divisão: ", divisao(10, 2))
